642213ccafed819add4e9bd9744e5c0be800502c
alam2025 <alam13100852@gmail.com>
2023-06-22 12:28:36 +0600

    rating

--- src/componets/PopularRecipeCard.jsx
+++ src/componets/PopularRecipeCard.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import LazyLoad from 'react-lazy-load';
 import { Link } from 'react-router-dom';
-
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+import Rating from 'react-rating';
 const PopularRecipeCard = ({ recipe }) => {
       const { food_image, name, rating, cooking_method, id
       } = recipe;
@@ -14,7 +15,15 @@ const PopularRecipeCard = ({ recipe }) => {
                   <div className=' px-4'>
                         <h1 className=' text-2xl font-semibold'>{name}</h1>
                         <p><span className='  font-semibold'>Cooking Method : </span> <br />{cooking_method.slice(0, 80)}... <Link to={`/recipe/${id}`} className=' text-rose-500'>See More</Link></p>
-                        <p>{rating}</p>
+                        <p className=' flex items-center'>
+                              <Rating
+                                    placeholderRating={rating}
+                                    readonly
+                                    emptySymbol={<AiOutlineStar size={25}/>}
+                                    placeholderSymbol={<AiFillStar className=' text-orange-400' size={23}/>}
+                                    fullSymbol={<AiFillStar className=' text-orange-400' size={25}/>}
+                              /> <span className=' text-xl font-semibold'>{rating}</span>
+                        </p>
                   </div>
 
             </div>
